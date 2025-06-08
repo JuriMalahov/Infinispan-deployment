629fdfa5208f6c6f4da8391a73625e18c1743bd2
JuriMalahov <144311634+JuriMalahov@users.noreply.github.com>
2025-06-08 18:51:36 +0300

    Add files via upload

--- configure_alt.sh
+++ configure_alt.sh
@@ -14,26 +14,29 @@ echo "default via 192.168.1.1" > /etc/net/ifaces/enp0s3/ipv4route
 
 echo "2. Перезапуск сервиса network"
 systemctl restart network
-systemctl restart network
 
-echo "3. Обновление списка пакетов и установка SSH-сервера"
+echo "3. Добавление записей в /etc/hosts"
+grep -q '192.168.1.2 astra' /etc/hosts || echo '192.168.1.2 astra' >> /etc/hosts
+grep -q '192.168.1.3 alt' /etc/hosts || echo '192.168.1.3 alt' >> /etc/hosts
+
+echo "4. Обновление списка пакетов и установка SSH-сервера"
 apt-get update && apt-get -y install openssh-server
 
-echo "4. Настройка sshd_config"
+echo "5. Настройка sshd_config"
 sed -i 's/^#\?Port .*/Port 22/' /etc/openssh/sshd_config
 sed -i 's/^#\?PermitRootLogin .*/PermitRootLogin yes/' /etc/openssh/sshd_config
 
-echo "5. Активация и перезапуск sshd"
-systemctl enable sshd
+echo "6. Перезапуск sshd"
+systemctl restart sshd
 
-echo "6. Генерация SSH-ключа"
+echo "7. Генерация SSH-ключа"
 ssh-keygen -N ""
 
-echo "7. Копирование ключей на хосты alt и astra"
-ssh-copy-id -o StrictHostKeyChecking=no "root@192.168.1.2"
-ssh-copy-id -o StrictHostKeyChecking=no "root@192.168.1.3"
+echo "8. Копирование ключей на хосты alt и astra"
+ssh-copy-id -o StrictHostKeyChecking=no "root@astra"
+ssh-copy-id -o StrictHostKeyChecking=no "root@alt"
 
-echo "8. Установка git и ansible"
+echo "9. Установка git и ansible"
 apt-get -y install git ansible
 
 echo "Настройка Alt Linux завершена."
